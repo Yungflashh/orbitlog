2047ac649921d4515f295f436e512c080841715d
Yungflashh <da-alquincy@gmail.com>
2024-09-23 12:40:31 +0100

    first commit

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ import env from "dotenv";
 import pg from "pg";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcryptjs";
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { collection } from "firebase/firestore";
-// import { addDoc } from "firebase/firestore";
-// import { getDocs } from "firebase/firestore";
-// import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -18,27 +12,19 @@ const port = 3000;
 env.config();
 
 app.use(express.json());
-// app.use(session({
-//     secret: process.env.MY_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//         secure: false,
-//         maxAge: 1000 * 10,
-//     }
-// }))
-
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// const db = new pg.Client({
-//     user: process.env.USER_DATABASE,
-//     host: process.env.HOST_DATABASE,
-//     database: process.env.DATABASE,
-//     password: process.env.PASSWORD_DATABASE,
-//     port: process.env.PORT_DATABASE,
-// })
+app.use(session({
+    secret: process.env.MY_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60,
+    }
+}))
 
 const db = new pg.Client({
     user: process.env.USER_DATABASE,
@@ -48,104 +34,94 @@ const db = new pg.Client({
     port: process.env.PORT_DATABASE,
 });
 
-
 db.connect();
-// console.log(db);
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAGtcYUKpook7Fzb4Z-9aD1QTeWlI5qJ0A",
-//     authDomain: "orbitlogs-12b2c.firebaseapp.com",
-//     projectId: "orbitlogs-12b2c",
-//     storageBucket: "orbitlogs-12b2c.appspot.com",
-//     messagingSenderId: "680093097811",
-//     appId: "1:680093097811:web:6f884d5dfba8f5a080cba6"
-//   };
 
-// const firebaseapp = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// const usersCollectionRef = collection(db, 'users');
-
-// function isAuthenticated(req){
-//     return req.session && req.session.user;
-// }
+function isAuthenticated(req){
+    return req.session && req.session.user;
+}
 
 app.get("/", (req, res) => {
     res.render("login.ejs");
 });
 
+app.get("/register", (req, res) => {
+    res.render("registerPage.ejs");
+});
+
 app.get("/dashboard", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("dashboard.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 app.get("/usa", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("usa.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 app.get("/shopWith", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("shopWith.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 app.get("/uk", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("uk.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 
 app.get("/others", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("others.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 
 });
 
 app.get("/company", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("company.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 app.get("/canada", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cookieValue = req.cookies["Current balance"];
         res.render("canada.ejs", {user: req.session.user, cookieValue}); 
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 app.get("/cart", (req, res) => {
-    // if(isAuthenticated(req)){
+    if(isAuthenticated(req)){
         const cart = req.cookies["CartArray"];
         if(cart){
             const array = JSON.parse(cart);
@@ -156,19 +132,64 @@ app.get("/cart", (req, res) => {
         else{
             res.render("cart.ejs", {user: req.session.user});     
         }
-    // }
-    // else{
-    //     res.redirect("/");
-    // }
+    }
+    else{
+        res.redirect("/");
+    }
 });
+
+app.get("/logout", (req, res) => {
+    res.redirect("/");
+})
+
+app.post("/register", async (req, res) => {
+    try{
+        const {
+            username: username,
+            email: email,
+            password: password,
+            checkPassword: checkPassword,
+        } = req.body;
+
+        console.log(req.body)
+    
+        // console.log(username)
+        const response = await db.query("SELECT * FROM orbitlog WHERE email = $1",
+            [email]
+        );
+        console.log(response.rows);
+        
+        if(response.rows.length > 0){
+            res.render("registerPage.ejs", {error: `Email "${response.rows[0].email}" has been used`})
+        }
+        else if(password.length < 6){
+            res.render("registerPage.ejs", {error: `Password length has to be greater than or equal to 6`})
+        }
+        else if(password != checkPassword){
+            res.render("registerPage.ejs", {error: `Your password characters needs to match`})
+        }
+        else if(response.rows.length === 0 && (password === checkPassword)){
+            const saltRounds = 10;
+            const hashPassword = await bcrypt.hash(password, saltRounds);
+            console.log(hashPassword);
+            const newUser = await db.query("INSERT INTO orbitlog (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+                [username, email, hashPassword]
+            )
+            console.log(newUser.rows[0]);
+            res.redirect("/");
+        }
+    }
+    catch(error){
+        console.error(error);
+    }
+})
 
 app.post("/dashboard", async(req, res) => {
     try{
         const {username, password} = req.body;
-        // const response = await db.query("SELECT * FROM defaultdb WHERE email = $1",
-        //     [username]
-        // );
-        const response = await db.query("SELECT * FROM defaultdb");
+        const response = await db.query("SELECT * FROM orbitlog WHERE email = $1",
+            [username]
+        );
         const userData = response.rows;
         // console.log(response.rows);
 
@@ -181,7 +202,6 @@ app.post("/dashboard", async(req, res) => {
                 email: userEmail,
                 password: storedPassword,
             } = userData[0];
-            console.log(userData[0])
 
             bcrypt.compare(password, storedPassword, (error, valid) => {
                 if(error){
@@ -190,12 +210,14 @@ app.post("/dashboard", async(req, res) => {
                 }
                 else{
                     if(valid){
-                        // req.session.user = name;
+                        req.session.user = userData[0].username;
+                        // console.log(req.session.user)
                         res.redirect("/dashboard")
                         console.log(valid);
                     }
                     else{
-                        res.render("login.ejs", {error: "Incorrect password"})
+                        res.render("login.ejs", {error: "Incorrect password"});
+                        console.log("Incorrect password");
                     }
                 }
             });
